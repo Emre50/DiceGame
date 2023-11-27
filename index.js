f05ce374a9221b627fa5f50d0a47382eec2bba31
Emre Deveci <116013389+Emre50@users.noreply.github.com>
2023-11-27 14:11:13 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var randomNumber1 = Math.floor(Math.random()*6 + 1);
+
+document.querySelector(".img1").setAttribute("src" , "./dice" + randomNumber1 + ".png");
+
+var randomNumber2 = Math.floor(Math.random()*7);
+
+document.querySelector(".img2").setAttribute("src" , "./dice" + randomNumber2 + ".png");
+
+if(randomNumber1 === randomNumber2){
+    document.querySelector("h1").innerHTML = "DRAW!";
+}
+
+else if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player1 Wins!";
+
+}
+else{
+    document.querySelector("h1").innerHTML = "Player2 Wins!";
+    
+}
